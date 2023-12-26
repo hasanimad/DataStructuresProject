855562b51d7437e215f9690ba7c0203d143cc1f6
sdds <hasanimad2004@gmail.com>
2023-12-26 14:31:21 +0300

    Finalized the Queue structure

--- main.c
+++ main.c
@@ -15,7 +15,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Headers/Queue.h"
-
+// testing grounds
 int main() {
     pQueue testQueue = newQueue();
     testQueue->enqueueData(testQueue, "Data 0x0");
