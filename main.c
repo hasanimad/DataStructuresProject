a0a09ba9140f91035792161c7c442b1a80bb54b8
sdds <hasanimad2004@gmail.com>
2023-12-26 14:30:03 +0300

    Finalized the Queue structure

--- main.c
+++ main.c
@@ -7,19 +7,20 @@
  * (Done) TODO Check time complexity (Queue)
  * (Done) TODO Implement the dequeue function
  * (Done) TODO Implement the updateData function in an ordered manner
- * TODO Check the iterate function, cause it looks wonky
+ * (DONE) TODO Check the iterate function, cause it looks wonky
  * TODO Implement the Undo Function
  * TODO Implement the Interface for the application
  */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "Headers/Queue.h"
+
 int main() {
     pQueue testQueue = newQueue();
     testQueue->enqueueData(testQueue, "Data 0x0");
     testQueue->enqueueData(testQueue, "Data 0x1");
     testQueue->iterQueue(testQueue);
     testQueue->delQueue(testQueue);
-
     return 0;
 }
